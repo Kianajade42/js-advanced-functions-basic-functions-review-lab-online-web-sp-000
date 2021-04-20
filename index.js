58d281a9d5de4af6c8023a94c7d431e4f314165b
Kiana Long <kianajade42.kl@gmail.com>
2021-04-20 16:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Calculator = {
   {return a + b;},
   subtract: function(a,b)
     {return a - b},
-  multiply: function(a,b) 
+  multiply: function(a,b)
     {return a * b},
   divide: function(a,b) 
     {return a / b;}
