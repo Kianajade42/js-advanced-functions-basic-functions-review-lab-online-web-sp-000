c85f951012ce54908e19da50e4744f07542484df
Kiana Long <kianajade42.kl@gmail.com>
2021-04-20 15:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Your code here
 function saturdayFun(activity="roller-skate")
 {return `This Saturday, I want to ${activity}!`}
+
+ const mondayWork=funcion()
+ return {`This Monday, I will ${activity}.`
+}
